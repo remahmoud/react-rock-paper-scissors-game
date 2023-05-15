fcd0f2169debb02f0515c8a74ec3c59c84b985a3
Mahmoud Ibrahiam <equalmahmoud@gmail.com>
2023-05-15 10:08:55 +0300

    choice list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import ChoiceList from "./components/ChoiceList";
+import Score from "./components/Score";
+
 export default function App() {
-    return <div></div>;
+    return (
+        <div className="container mx-auto px-28 h-screen grid place-content-center">
+            <h1 className="text-6xl font-bold mb-8">Rock, Paper, Scissors</h1>
+            <Score />
+            <ChoiceList />
+        </div>
+    );
 }
